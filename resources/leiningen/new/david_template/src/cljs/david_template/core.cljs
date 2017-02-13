e8e56ea22c46d4579d1e9dfe8c46e0a99a8e06bd
David Badke <davidrbadke@gmail.com>
2017-02-12 20:17:48 -1000

    change example

--- resources/leiningen/new/david_template/src/cljs/david_template/core.cljs
+++ resources/leiningen/new/david_template/src/cljs/david_template/core.cljs
@@ -1,40 +1,18 @@
 (ns {{name}}.core
-  (:require [reagent.core :as reagent]
-            [re-frame.core :as re-frame]))
+  (:require [reagent.core :as r])
+  (:require-macros [devcards.core :refer [defcard-rg]]))
 
-(enable-console-print!)
+(defn simple-component []
+  [:div.simple-component 
+    [:p "I am a component!"]
+    [:p "I have "
+        [:span.simple-component__bold "bold "]
+        [:span.simple-component__red "and red "]
+        "text!"]])
 
-(re-frame/reg-sub   ;; the means by which main-panel gets data
-  :name             ;; usage (subscribe [:name])
-  (fn  [db _]
-    (:display-name db)))
+(defcard-rg simple-component-card
+  [simple-component])
 
-(re-frame/reg-sub        ;; we can check if there is data
-  :initialised?          ;; usage (subscribe [:initialised?])
-  (fn  [db _]
-    (not (empty? db))))  ;; do we have data
-
-(re-frame/reg-event-db
-   :initialise-db
-   (fn [db _]
-       (assoc db :display-name "Jane Doe")))
-
-(defn main-panel    ;; the top level of our app 
-  []
-  (let [name  (re-frame/subscribe [:name])]   ;; we need there to be good data
-    [:div "Hello " @name]))
-
-(defn top-panel    ;; this is new
-  []
-  (let [ready?  (re-frame/subscribe [:initialised?])]
-    (if-not @ready?         ;; do we have good data?
-      [:div "Initialising ..."]   ;; tell them we are working on it
-      [main-panel])))      ;; all good, render this component
-
-(defn ^:export main     ;; call this to bootstrap your app
-  []
-  (re-frame/dispatch [:initialise-db])
-  (reagent/render [top-panel]
-                  (js/document.getElementById "app")))
-
-(main)
+(defn ^:export main []
+  (r/render [simple-component]
+            (js/document.getElementById "app")))
